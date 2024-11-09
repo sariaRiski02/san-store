0aab09a6963d8df1eb50fe4a45e071b4a68d7943
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2024-11-10 01:26:33 +0800

    add unitTest for login endpoint

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -6,15 +6,35 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use function PHPUnit\Framework\assertSame;
+
 class ApiTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function testLoginSuccess(): void
     {
-        $response = $this->get('/');
-
+        $response = $this->post("/api/login", [
+            "email" => "test@example.com",
+            "password" => "test123"
+        ]);
         $response->assertStatus(200);
+        $this->assertSame($response["status"], "success");
+    }
+
+    public function testLoginFailed()
+    {
+
+        // test missing password
+        $responseMissingPassword = $this->post("/api/login", [
+            "email" => "test@example.com",
+        ]);
+        $this->assertSame($responseMissingPassword["status"], "error");
+        // test missing password
+        $responseMissingEmail = $this->post("/api/login", [
+            "password" => "test123",
+        ]);
+        $this->assertSame($responseMissingEmail["status"], "error");
     }
 }
