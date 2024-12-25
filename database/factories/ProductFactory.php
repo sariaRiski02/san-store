ce7a63756fe6638b76c00990c0994b821bfe8051
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2024-12-25 23:35:59 +0800

    add model, factory and seeder class for product and category. And relation beetwen of them

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'code_item' => fake()->unique()->randomNumber(),
+            'price' => fake()->randomFloat(2, 1, 1000),
+            'description' => fake()->sentence(),
+            'category_id' => Category::inrandomOrder()->first()->id,
+        ];
+    }
+}
