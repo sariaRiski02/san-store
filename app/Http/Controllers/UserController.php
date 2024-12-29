e5c05e3aa07aa4a287e3a04d3e9bb92a1aec2974
sariaRiski02 <muhammatsaria026@student.unsrat.ac.id>
2024-12-29 12:15:37 +0800

    make registration code fixed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -15,9 +16,13 @@ class UserController extends Controller
             'name' => 'required|string',
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed',
-            'code_register' => 'required|string',
+            'code_register' => 'string|nullable',
         ]);
 
+        $role = 'user';
+        if ($request->input('registration_code') === env('REGISTRATION_CODE')) {
+            $role = 'admin';
+        }
         // Jika validasi gagal
         if ($validator->fails()) {
             return response()->json([
@@ -32,6 +37,7 @@ class UserController extends Controller
             $user = User::create([
                 'name' => $request->input('name'),
                 'email' => $request->input('email'),
+                'role' => $role,
                 'password' => bcrypt($request->input('password')),
             ]);
             // Respon sukses
@@ -51,5 +57,34 @@ class UserController extends Controller
     }
 
 
-    public function login() {}
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email,exist',
+            'password' => 'required|string|'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors(),
+            ]);
+        }
+
+        $login = Auth::attempt([
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ]) && Auth::user()->role === 'admin';
+
+        if ($login) {
+            $token = $request->user()->createToken('authToken')->plainTextToken;
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'data' => Auth::user(),
+                'token' => $token,
+            ]);
+        }
+    }
 }
